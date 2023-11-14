9bc8dd404166256f39113cd705a6d9d2e625b67f
liyibo <614693063@qq.com>
2023-11-14 20:40:30 +0800

    使用第一个ip核，写了rom_tb测试文件

---
+++ vivadoProject/rom_test/rom_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ns
+
+module rom_tb ();
+    
+
+    reg clka;
+    reg [9:0] addra;    // ip核中定义为10位，如果定义为8位，输出就是高阻态
+    wire [9:0] douta;
+
+    rom rom_inst(
+        .clka(clka),
+        .addra(addra),
+        .douta(douta)
+    );
+
+    initial clka = 0;
+    always #10 clka = ~clka;
+
+    initial begin
+        addra = 100;
+        #501;
+        repeat(3000) begin
+            addra = addra + 1;
+            #20;
+        end
+        #200000;
+        $stop;
+    end
+
+endmodule
