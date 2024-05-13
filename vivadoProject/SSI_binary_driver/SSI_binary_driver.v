219b9dfffdfeee37cf979424b7c3b9714615c6a9
pychego <614693063@qq.com>
2024-05-13 10:15:46 +0800

    添加了param_distributor的ip代码, 添加了ssi二进制驱动

--- vivadoProject/SSI_binary_driver/SSI_binary_driver.v
+++ vivadoProject/SSI_binary_driver/SSI_binary_driver.v
@@ -1,5 +1,6 @@
 // what is the position of this module in the entire design?
-module SSI_gray_driver (
+// 为了方便与AXI总线传输, 将输出设置成32bit, 但是实际只有25bit有用
+module SSI_binary_driver (
     input clk,
     input rst_n,
     (*mark_DEBUG = "TRUE"*) input SSI_data,            // 传感器差分信号经MAX3077变为单端信号，给zynq
@@ -81,61 +82,7 @@ module SSI_gray_driver (
         end
     end
 
-    reg [ 4:0] i = 0;
-    (*mark_DEBUG = "TRUE"*)reg [24:0] loc_data_binary;
-    (*mark_DEBUG = "TRUE"*)reg [24:0] loc_data_binary_r;
-    (*mark_DEBUG = "TRUE"*)reg [24:0] loc_data_gray_r;
-
-    // ???
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) i <= 5'd0;
-        else begin
-            i <= i + 1'b1;
-            case (i)  // gray格雷码 to binary principle
-                // Force Serial 其实是强制转换为串行操作了，
-                // 看仿真知道高的代码loc_data_binary_r也有暂态， 原因是在下面的转换过程中loc_data_gray也可能会变化
-                // 一下格雷码 ==> 二进制 是绝对没问题的, 已经分析过仿真波形了, 正确的
-                5'd0: begin
-                    loc_data_binary[24] <= loc_data_gray[24];
-                    loc_data_gray_r <= loc_data_gray;
-                end
-                5'd1: loc_data_binary[23] <= loc_data_binary[24] ^ loc_data_gray_r[23];
-                5'd2: loc_data_binary[22] <= loc_data_binary[23] ^ loc_data_gray_r[22];
-                5'd3: loc_data_binary[21] <= loc_data_binary[22] ^ loc_data_gray_r[21];
-                5'd4: loc_data_binary[20] <= loc_data_binary[21] ^ loc_data_gray_r[20];
-                5'd5: loc_data_binary[19] <= loc_data_binary[20] ^ loc_data_gray_r[19];
-                5'd6: loc_data_binary[18] <= loc_data_binary[19] ^ loc_data_gray_r[18];
-                5'd7: loc_data_binary[17] <= loc_data_binary[18] ^ loc_data_gray_r[17];
-                5'd8: loc_data_binary[16] <= loc_data_binary[17] ^ loc_data_gray_r[16];
-                5'd9: loc_data_binary[15] <= loc_data_binary[16] ^ loc_data_gray_r[15];
-                5'd10: loc_data_binary[14] <= loc_data_binary[15] ^ loc_data_gray_r[14];
-                5'd11: loc_data_binary[13] <= loc_data_binary[14] ^ loc_data_gray_r[13];
-                5'd12: loc_data_binary[12] <= loc_data_binary[13] ^ loc_data_gray_r[12];
-                5'd13: loc_data_binary[11] <= loc_data_binary[12] ^ loc_data_gray_r[11];
-                5'd14: loc_data_binary[10] <= loc_data_binary[11] ^ loc_data_gray_r[10];
-                5'd15: loc_data_binary[9] <= loc_data_binary[10] ^ loc_data_gray_r[9];
-                5'd16: loc_data_binary[8] <= loc_data_binary[9] ^ loc_data_gray_r[8];
-                5'd17: loc_data_binary[7] <= loc_data_binary[8] ^ loc_data_gray_r[7];
-                5'd18: loc_data_binary[6] <= loc_data_binary[7] ^ loc_data_gray_r[6];
-                5'd19: loc_data_binary[5] <= loc_data_binary[6] ^ loc_data_gray_r[5];
-                5'd20: loc_data_binary[4] <= loc_data_binary[5] ^ loc_data_gray_r[4];
-                5'd21: loc_data_binary[3] <= loc_data_binary[4] ^ loc_data_gray_r[3];
-                5'd22: loc_data_binary[2] <= loc_data_binary[3] ^ loc_data_gray_r[2];
-                5'd23: loc_data_binary[1] <= loc_data_binary[2] ^ loc_data_gray_r[1];
-                5'd24: loc_data_binary[0] <= loc_data_binary[1] ^ loc_data_gray_r[0];
-                5'd25: begin
-                    // 这个地方是为了解决loc_data_binary的不稳定状态
-                    // 加了一个时序和一个寄存器， 保证loc_data_binary_r存放的都是稳定的位移数据
-                    // 看loc_data_binary_r改变的频率就是位移数据改变的频率了
-                    loc_data_binary_r <= loc_data_binary;
-                    i <= 5'd0;
-                end
-                default: i <= 5'd0;
-            endcase
-        end
-    end
-
-    assign loc_data = loc_data_binary_r;
+    assign loc_data = loc_data_gray;
 
     // 需不需要设置复位？ 感觉不需要
 
---
+++ vivadoProject/SSI_binary_driver/SSI_binary_driver_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module SSI_binary_driver_tb ();
+
+    reg         clk;
+    reg         rst_n;
+    reg         SSI_data;
+    wire        SSI_clk;
+    wire [24:0] loc_data;
+
+    initial begin
+        clk   = 1'b0;
+        rst_n = 1'b0;
+        #15 rst_n = 1'b1;
+    end
+
+    // 100Mhz
+    always #5 clk = ~clk;
+
+    always @(posedge SSI_clk) begin
+        // $random()生成一个32位随机数，取模2得到0或1
+        SSI_data = $random() % 2;
+    end
+
+    initial begin
+        #300000 $finish;
+    end
+
+    SSI_binary_driver u_SSI_binary_driver (
+        .clk     (clk),
+        .rst_n   (rst_n),
+        .SSI_data(SSI_data),
+        .SSI_clk (SSI_clk),     // output to sensor
+        .loc_data(loc_data)     // output
+    );
+
+endmodule
