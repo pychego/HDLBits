3fce814e227cc79cbf13c3d354bd366ff1c6f741
pychego <614693063@qq.com>
2024-11-22 16:45:40 +0800

    优化后的六ssi经过了验证

--- vivadoProject/SSI_binary_driver/Six_SSI_binary_driver.v
+++ vivadoProject/SSI_binary_driver/Six_SSI_binary_driver.v
@@ -1,5 +1,16 @@
-// 完全复制SSI_binary_driver.v而来
-// 适用于紫金港六自由度平台
+/*  2024.11.22 规范了代码写法, 复习了SSI驱动
+端口: 
+    SSI_data  传感器差分信号经MAX3077变为单端信号，给zynq
+    SSI_clk   输出(单端) => MAX3077(差分) => 传感器
+    loc_data  并行位移数据, 为了与后面后面接口一致,用0扩充到32bits,仅低25bit有用
+
+原理:
+    根据SSI时序可知，SSI_clk每来一个上升沿,传感器就送入一个SSI_data 因此要设计在SSI_clk下降沿读取SSI_data
+    通过25个SSI_clk的下降沿, 读取25个串行SSI_data
+时序:
+    通过时序图可知 loc_data 大概73us更新一次，1ms内更新13次，完全够用的; 
+*/
+
 module Six_SSI_binary_driver (
     input clk,
     input rst_n,
@@ -11,11 +22,11 @@ module Six_SSI_binary_driver (
     input SSI_data5,
 
     output reg SSI_clk0,  // output to sensor
-    output     SSI_clk1,  // output to sensor
-    output     SSI_clk2,  // output to sensor
-    output     SSI_clk3,  // output to sensor
-    output     SSI_clk4,  // output to sensor
-    output     SSI_clk5,  // output to sensor
+    output reg SSI_clk1,  // output to sensor
+    output reg SSI_clk2,  // output to sensor
+    output reg SSI_clk3,  // output to sensor
+    output reg SSI_clk4,  // output to sensor
+    output reg SSI_clk5,  // output to sensor
 
     output [31:0] loc_data0,  // 31bit, 为了和后面的模块数据对齐
     output [31:0] loc_data1,
@@ -24,9 +35,8 @@ module Six_SSI_binary_driver (
     output [31:0] loc_data4,
     output [31:0] loc_data5
 );
-    // 根据SSI时序可知，SSI_clk0每来一个上升沿传感器就送入一个SSI_data0 因此要设计在SSI_clk0下降沿读取SSI_data0
 
-    // 根据cnt计数设计1MHz信号
+    // 根据cnt计数设计1MHz信号(实验室开发板时钟为100Mhz)
     reg [7:0] cnt;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) cnt <= 8'd0;
@@ -37,29 +47,66 @@ module Six_SSI_binary_driver (
     wire clk_1MHz_en;
     assign clk_1MHz_en = (cnt == 8'd1);
 
-    // count_1MHz from 0 to 72, 73 states in total
+    // count_1MHz 从0到72循环, 一共73个状态
     // count_1MHz在每个状态停留的时间是 1 / 1MHz
     reg [7:0] count_1MHz;
-    always @(posedge clk or negedge rst_n) begin
+    always @(posedge clk, negedge rst_n) begin
         if (!rst_n) count_1MHz <= 8'd0;
         else if (clk_1MHz_en) begin
-            count_1MHz <= count_1MHz + 1'b1;
-            if (count_1MHz > 8'd71) count_1MHz <= 8'd0;
+            if (count_1MHz > 8'd71) count_1MHz <= 0;
+            else count_1MHz <= count_1MHz + 1;
         end
     end
 
-    // SSI_clk0's frequency is 1Mhz / 2 = 500kHz
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) SSI_clk0 <= 1'b1;
-        else if (clk_1MHz_en) begin
-            if (count_1MHz < 20) SSI_clk0 <= 1'b1;
-            else if (count_1MHz == 20) SSI_clk0 <= 1'b0;
-            else if (count_1MHz > 20 && count_1MHz <= 71) SSI_clk0 <= ~SSI_clk0;
-            else SSI_clk0 <= SSI_clk0;
+    /*  SSI_clk's frequency is 1Mhz / 2 = 500kHz
+        if (count_1MHz > 20 && count_1MHz <= 71) SSI_clk翻转了51次
+        由于第一次翻转前是0, 翻转51次产生了25个上升沿, 同时最后保持高电平1
+    */
+    always @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            SSI_clk0 <= 1'b1;
+            SSI_clk1 <= 1'b1;
+            SSI_clk2 <= 1'b1;
+            SSI_clk3 <= 1'b1;
+            SSI_clk4 <= 1'b1;
+            SSI_clk5 <= 1'b1;
+        end else if (clk_1MHz_en) begin
+            if (count_1MHz < 20) begin
+                SSI_clk0 <= 1'b1;
+                SSI_clk1 <= 1'b1;
+                SSI_clk2 <= 1'b1;
+                SSI_clk3 <= 1'b1;
+                SSI_clk4 <= 1'b1;
+                SSI_clk5 <= 1'b1;
+            end else if (count_1MHz == 20) begin
+                SSI_clk0 <= 1'b0;
+                SSI_clk1 <= 1'b0;
+                SSI_clk2 <= 1'b0;
+                SSI_clk3 <= 1'b0;
+                SSI_clk4 <= 1'b0;
+                SSI_clk5 <= 1'b0;
+            end else if (count_1MHz > 20 && count_1MHz <= 71) begin
+                SSI_clk0 <= ~SSI_clk0;
+                SSI_clk1 <= ~SSI_clk1;
+                SSI_clk2 <= ~SSI_clk2;
+                SSI_clk3 <= ~SSI_clk3;
+                SSI_clk4 <= ~SSI_clk4;
+                SSI_clk5 <= ~SSI_clk5;
+            end else begin
+                SSI_clk0 <= SSI_clk0;
+                SSI_clk1 <= SSI_clk1;
+                SSI_clk2 <= SSI_clk2;
+                SSI_clk3 <= SSI_clk3;
+                SSI_clk4 <= SSI_clk4;
+                SSI_clk5 <= SSI_clk5;
+            end
         end
     end
 
-    // SSI_flag is used to indicate the SSI_clk0 is in the period of 22~71
+    /* SSI_flag is used to indicate the SSI_clk is in the period of 22~71
+       SSI_flag 用来指示这段时间的SSI_clk的下降沿可用, 是用来取data的,
+       SSI_flag 在22_71状态下为1, 这段时间有25个完整的下降沿
+    */
     reg SSI_flag;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) SSI_flag <= 1'b0;
@@ -71,30 +118,26 @@ module Six_SSI_binary_driver (
         end
     end
 
-    // 25 SSI_clk0 neg edge
+    // 25 SSI_clk neg edge
     reg [7:0] count_SSIclk;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    always @(negedge SSI_clk0, negedge rst_n) begin
         if (!rst_n) count_SSIclk <= 8'd0;
         else if (SSI_flag) count_SSIclk <= count_SSIclk + 1'b1;
         else if (count_SSIclk == 8'd25) count_SSIclk <= 8'd0;
     end
+    // 以上为时钟设置
 
-    assign SSI_clk1 = SSI_clk0;
-    assign SSI_clk2 = SSI_clk0;
-    assign SSI_clk3 = SSI_clk0;
-    assign SSI_clk4 = SSI_clk0;
-    assign SSI_clk5 = SSI_clk0;
-    /* 以上为时钟输出信号的设置 ****************************************/
+
 
 
     /* 设置通道输出 loc_data0 ****************************************/
-    (*mark_DEBUG = "TRUE"*)reg [24:0] data_buffer0;
-    (*mark_DEBUG = "TRUE"*)reg [24:0] loc_data0_gray;
+    reg [24:0] data_buffer0;
+    reg [24:0] loc_data_25bit0;
     // SSI_data0 in the negedge od SSI_clk0 is stable
     // 由于SSI_clk 6个完全相同,这里直接统一用了SSI_clk0
     always @(negedge SSI_clk0 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data0_gray <= 25'd0;
+            loc_data_25bit0 <= 25'd0;
             data_buffer0   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
@@ -102,116 +145,111 @@ module Six_SSI_binary_driver (
         end else if (count_SSIclk == 8'd25) begin
             // time is count_SSIclk from 25 to 0
             // 如果是二进制的位移传感器, 那得到loc_data0_gray程序就结束了
-            loc_data0_gray <= data_buffer0;
+            loc_data_25bit0 <= data_buffer0;
             data_buffer0   <= 25'd0;
         end
     end
 
-    // 这里使用了低位宽数据给高位宽数据赋值,应该是没啥问题
-    assign loc_data0 = loc_data0_gray;
+    assign loc_data0 = {{7{1'b0}}, loc_data_25bit0};
 
 
     /* 设置通道输出 loc_data1 ****************************************/
-    (*mark_DEBUG = "TRUE"*)reg [24:0] data_buffer1;
-    (*mark_DEBUG = "TRUE"*)reg [24:0] loc_data1_gray;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    reg [24:0] data_buffer1;
+    reg [24:0] loc_data_25bit1;
+    always @(negedge SSI_clk1 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data1_gray <= 25'd0;
+            loc_data_25bit1 <= 25'd0;
             data_buffer1   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
             data_buffer1 <= {data_buffer1[23:0], SSI_data1};
         end else if (count_SSIclk == 8'd25) begin
-            loc_data1_gray <= data_buffer1;
+            loc_data_25bit1 <= data_buffer1;
             data_buffer1   <= 25'd0;
         end
     end
 
-    assign loc_data1 = loc_data1_gray;
+    assign loc_data1 = {{7{1'b0}}, loc_data_25bit1};
 
 
     /* 设置通道输出 loc_data2 ****************************************/
     reg [24:0] data_buffer2;
-    reg [24:0] loc_data2_gray;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    reg [24:0] loc_data_25bit2;
+    always @(negedge SSI_clk2 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data2_gray <= 25'd0;
+            loc_data_25bit2 <= 25'd0;
             data_buffer2   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
             data_buffer2 <= {data_buffer2[23:0], SSI_data2};
         end else if (count_SSIclk == 8'd25) begin
-            loc_data2_gray <= data_buffer2;
+            loc_data_25bit2 <= data_buffer2;
             data_buffer2   <= 25'd0;
         end
     end
 
-    assign loc_data2 = loc_data2_gray;
+    assign loc_data2 = {{7{1'b0}}, loc_data_25bit2};
 
 
     /* 设置通道输出 loc_data3 ****************************************/
     reg [24:0] data_buffer3;
-    reg [24:0] loc_data3_gray;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    reg [24:0] loc_data_25bit3;
+    always @(negedge SSI_clk3 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data3_gray <= 25'd0;
+            loc_data_25bit3 <= 25'd0;
             data_buffer3   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
             data_buffer3 <= {data_buffer3[23:0], SSI_data3};
         end else if (count_SSIclk == 8'd25) begin
-            loc_data3_gray <= data_buffer3;
+            loc_data_25bit3 <= data_buffer3;
             data_buffer3   <= 25'd0;
         end
     end
 
-    assign loc_data3 = loc_data3_gray;
+    assign loc_data3 = {{7{1'b0}}, loc_data_25bit3};
+
 
     /* 设置通道输出 loc_data4 ****************************************/
     reg [24:0] data_buffer4;
-    reg [24:0] loc_data4_gray;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    reg [24:0] loc_data_25bit4;
+    always @(negedge SSI_clk4 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data4_gray <= 25'd0;
+            loc_data_25bit4 <= 25'd0;
             data_buffer4   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
             data_buffer4 <= {data_buffer4[23:0], SSI_data4};
         end else if (count_SSIclk == 8'd25) begin
-            loc_data4_gray <= data_buffer4;
+            loc_data_25bit4 <= data_buffer4;
             data_buffer4   <= 25'd0;
         end
     end
 
-    assign loc_data4 = loc_data4_gray;
+    assign loc_data4 = {{7{1'b0}}, loc_data_25bit4};
+
 
     /* 设置通道输出 loc_data5 ****************************************/
     reg [24:0] data_buffer5;
-    reg [24:0] loc_data5_gray;
-    always @(negedge SSI_clk0 or negedge rst_n) begin
+    reg [24:0] loc_data_25bit5;
+    always @(negedge SSI_clk5 or negedge rst_n) begin
         if (!rst_n) begin
-            loc_data5_gray <= 25'd0;
+            loc_data_25bit5 <= 25'd0;
             data_buffer5   <= 25'd0;
         end else if (SSI_flag) begin
             // 新来的放到最低位，旧的左移一位
             data_buffer5 <= {data_buffer5[23:0], SSI_data5};
         end else if (count_SSIclk == 8'd25) begin
-            loc_data5_gray <= data_buffer5;
+            loc_data_25bit5 <= data_buffer5;
             data_buffer5   <= 25'd0;
         end
     end
 
-    assign loc_data5 = loc_data5_gray;
-
+    assign loc_data5  = {{7{1'b0}}, loc_data_25bit5};
 
 endmodule
 /*
-此为二进制传感器的驱动模块，输出是二进制的位移数据[24:0] loc_data0 但我需要知道这25位代表的实际位移是多少
-
-我改动的部分，在case刚进入0的时候，保存下来loc_data0_gray为loc_data0_gray_r，在case的0-25这一轮，loc_data0_gray_r
-保持不变，这样可保证loc_data0_binary_r不会出现不稳定的暂态
-
-通过时序图可知 loc_data0 大概73us更新一次，1ms内更新13次，完全够用的; loc_data0虽然一直在变, 但是根据BRAM_wr_controller
-的时序要求, 是从bram_we=1的期间写入该时刻的 loc_data0
+loc_data虽然一直在变, 但是根据BRAM_wr_controller
+的时序要求, 是从bram_we=1的期间写入该时刻的 loc_data
 */
 
---
+++ vivadoProject/SSI_binary_driver/Six_SSI_binary_driver_tb.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+
+module SSI_binary_driver_tb ();
+
+    reg         clk;
+    reg         rst_n;
+    reg         SSI_data0;
+    reg         SSI_data1;
+    reg         SSI_data2;
+    reg         SSI_data3;
+    reg         SSI_data4;
+    reg         SSI_data5;
+    wire        SSI_clk0;
+    wire        SSI_clk1;
+    wire        SSI_clk2;
+    wire        SSI_clk3;
+    wire        SSI_clk4;
+    wire        SSI_clk5;
+    wire [31:0] loc_data0;
+    wire [31:0] loc_data1;
+    wire [31:0] loc_data2;
+    wire [31:0] loc_data3;
+    wire [31:0] loc_data4;
+    wire [31:0] loc_data5;
+
+    initial begin
+        clk   = 1'b0;
+        rst_n = 1'b0;
+        #15 rst_n = 1'b1;
+    end
+
+    // 100Mhz
+    always #5 clk = ~clk;
+
+    always @(posedge SSI_clk0) begin
+        // $random()生成一个32位随机数，取模2得到0或1
+        SSI_data0 = $random() % 2;
+        SSI_data1 = $random() % 3;
+        SSI_data2 = $random() % 4;
+        SSI_data3 = $random() % 5;
+        SSI_data4 = $random() % 6;
+        SSI_data5 = $random() % 7;
+    end
+
+    initial begin
+        #300000 $finish;
+    end
+
+    Six_SSI_binary_driver u_Six_SSI_binary_driver (
+        .clk      (clk),
+        .rst_n    (rst_n),
+        .SSI_data0(SSI_data0),
+        .SSI_data1(SSI_data1),
+        .SSI_data2(SSI_data2),
+        .SSI_data3(SSI_data3),
+        .SSI_data4(SSI_data4),
+        .SSI_data5(SSI_data5),
+        .SSI_clk0 (SSI_clk0),   // output to sensor
+        .SSI_clk1 (SSI_clk1),   // output to sensor
+        .SSI_clk2 (SSI_clk2),   // output to sensor
+        .SSI_clk3 (SSI_clk3),   // output to sensor
+        .SSI_clk4 (SSI_clk4),   // output to sensor
+        .SSI_clk5 (SSI_clk5),   // output to sensor
+        .loc_data0(loc_data0),  // output
+        .loc_data1(loc_data1),  // output
+        .loc_data2(loc_data2),  // output
+        .loc_data3(loc_data3),  // output
+        .loc_data4(loc_data4),  // output
+        .loc_data5(loc_data5)   // output
+    );
+
+endmodule
